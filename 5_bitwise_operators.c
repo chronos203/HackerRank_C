d747811e971da6d135f4cf24324a7f7dbf69be61
Tushar Kawadkar <chronos2.03@gmail.com>
2019-11-17 12:48:15 +0530

    5 - Bitwise Operators

--- 5_bitwise_operators.c
+++ 5_bitwise_operators.c
@@ -6,17 +6,17 @@ void calculate_maximum(int n, int k)
     int max_OR = 0;
     int max_XOR = 0;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = i + 1; j < n; j++)
+        for (int j = i + 1; j <= n; j++)
         {
-            if(( ((i & j) > max_AND)) && ((i & j) < k))
+            if(((i & j) > max_AND) && ((i & j) < k))
                 max_AND = i & j;
 
-            if(( ((i | j) > max_OR)) && ((i | j) < k))
+            if(((i | j) > max_OR) && ((i | j) < k))
                 max_OR = i | j;
 
-            if(( ((i ^ j) > max_XOR)) && ((i ^ j) < k))
+            if(((i ^ j) > max_XOR) && ((i ^ j) < k))
                 max_XOR = i ^ j;            
         }
     }
